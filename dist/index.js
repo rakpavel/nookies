35fd7f23b57542ef246591c270144cdcc50c018e
Pavel Rak <rak.pavel.89@gmail.com>
2019-09-25 14:16:29 +0200

    preserve newest cookies across parse cookie calls

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,25 @@
-import * as cookie from 'cookie'
-import * as next from 'next'
-import * as setCookieParser from 'set-cookie-parser'
-import { Cookie } from 'set-cookie-parser'
-import { CookieSerializeOptions } from 'cookie'
-
-const isBrowser = () => typeof window !== 'undefined'
-
+'use strict'
+var __assign =
+  (this && this.__assign) ||
+  function() {
+    __assign =
+      Object.assign ||
+      function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+          s = arguments[i]
+          for (var p in s)
+            if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p]
+        }
+        return t
+      }
+    return __assign.apply(this, arguments)
+  }
+Object.defineProperty(exports, '__esModule', { value: true })
+var cookie = require('cookie')
+var setCookieParser = require('set-cookie-parser')
+var isBrowser = function() {
+  return typeof window !== 'undefined'
+}
 /**
  * Compare the cookie and return true if the cookies has equivalent
  * options and the cookies would be overwritten in the browser storage.
@@ -13,7 +27,7 @@ const isBrowser = () => typeof window !== 'undefined'
  * @param a first Cookie for comparision
  * @param b second Cookie for comparision
  */
-function areCookiesEqual(a: Cookie, b: Cookie & CookieSerializeOptions) {
+function areCookiesEqual(a, b) {
   return (
     a.name === b.name &&
     a.domain === b.domain &&
@@ -22,7 +36,6 @@ function areCookiesEqual(a: Cookie, b: Cookie & CookieSerializeOptions) {
     a.secure === b.secure
   )
 }
-
 /**
  * Create an instance of the Cookie interface
  *
@@ -30,11 +43,7 @@ function areCookiesEqual(a: Cookie, b: Cookie & CookieSerializeOptions) {
  * @param value value of the Cookie
  * @param options Cookie options
  */
-function createCookie(
-  name: string,
-  value: string,
-  options: CookieSerializeOptions,
-): Cookie {
+function createCookie(name, value, options) {
   return {
     name: name,
     expires: options.expires,
@@ -46,7 +55,6 @@ function createCookie(
     path: options.path,
   }
 }
-
 /**
  *
  * Parses cookies.
@@ -54,42 +62,33 @@ function createCookie(
  * @param ctx
  * @param options
  */
-export function parseCookies(
-  ctx?: next.NextContext | null | undefined,
-  options?: cookie.CookieParseOptions,
-) {
+function parseCookies(ctx, options) {
   if (ctx && ctx.req && ctx.req.headers && ctx.req.headers.cookie) {
-    let currentCookies = {}
-
+    var currentCookies = {}
     if (ctx.res && ctx.res.getHeader) {
-      let cookies = ctx.res.getHeader('Set-Cookie') || []
-
+      var cookies = ctx.res.getHeader('Set-Cookie') || []
       if (typeof cookies === 'string') cookies = [cookies]
       if (typeof cookies === 'number') cookies = []
-
-      const parsedCookies = setCookieParser.parse(cookies)
-
-      currentCookies = parsedCookies.reduce((acc, cookie) => {
-        return {
-          ...acc,
-          [cookie.name]: cookie.value,
-        }
+      var parsedCookies = setCookieParser.parse(cookies)
+      currentCookies = parsedCookies.reduce(function(acc, cookie) {
+        var _a
+        return __assign(
+          __assign({}, acc),
+          ((_a = {}), (_a[cookie.name] = cookie.value), _a),
+        )
       }, {})
     }
-
     return Object.assign(
-      cookie.parse(ctx.req.headers.cookie as string, options),
+      cookie.parse(ctx.req.headers.cookie, options),
       currentCookies,
     )
   }
-
   if (isBrowser()) {
     return cookie.parse(document.cookie, options)
   }
-
   return {}
 }
-
+exports.parseCookies = parseCookies
 /**
  *
  * Sets a cookie.
@@ -99,24 +98,16 @@ export function parseCookies(
  * @param value
  * @param options
  */
-export function setCookie(
-  ctx: next.NextContext | null | undefined,
-  name: string,
-  value: string,
-  options: cookie.CookieSerializeOptions,
-) {
+function setCookie(ctx, name, value, options) {
   if (ctx && ctx.res && ctx.res.getHeader && ctx.res.setHeader) {
-    let cookies = ctx.res.getHeader('Set-Cookie') || []
-
+    var cookies = ctx.res.getHeader('Set-Cookie') || []
     if (typeof cookies === 'string') cookies = [cookies]
     if (typeof cookies === 'number') cookies = []
-
-    const parsedCookies = setCookieParser.parse(cookies)
-
-    let cookiesToSet: string[] = []
-    parsedCookies.forEach((parsedCookie: Cookie) => {
+    var parsedCookies = setCookieParser.parse(cookies)
+    var cookiesToSet_1 = []
+    parsedCookies.forEach(function(parsedCookie) {
       if (!areCookiesEqual(parsedCookie, createCookie(name, value, options))) {
-        cookiesToSet.push(
+        cookiesToSet_1.push(
           cookie.serialize(parsedCookie.name, parsedCookie.value, {
             domain: parsedCookie.domain,
             path: parsedCookie.path,
@@ -128,18 +119,15 @@ export function setCookie(
         )
       }
     })
-
-    cookiesToSet.push(cookie.serialize(name, value, options))
-    ctx.res.setHeader('Set-Cookie', cookiesToSet)
+    cookiesToSet_1.push(cookie.serialize(name, value, options))
+    ctx.res.setHeader('Set-Cookie', cookiesToSet_1)
   }
-
   if (isBrowser()) {
     document.cookie = cookie.serialize(name, value, options)
   }
-
   return {}
 }
-
+exports.setCookie = setCookie
 /**
  *
  * Destroys a cookie with a particular name.
@@ -148,33 +136,24 @@ export function setCookie(
  * @param name
  * @param options
  */
-export function destroyCookie(
-  ctx: next.NextConfig | null | undefined,
-  name: string,
-  options?: cookie.CookieSerializeOptions,
-) {
-  const opts = { ...(options || {}), maxAge: -1 }
-
+function destroyCookie(ctx, name, options) {
+  var opts = __assign(__assign({}, options || {}), { maxAge: -1 })
   if (ctx && ctx.res && ctx.res.setHeader && ctx.res.getHeader) {
-    let cookies = ctx.res.getHeader('Set-Cookie') || []
-
+    var cookies = ctx.res.getHeader('Set-Cookie') || []
     if (typeof cookies === 'string') cookies = [cookies]
     if (typeof cookies === 'number') cookies = []
-
     cookies.push(cookie.serialize(name, '', opts))
-
     ctx.res.setHeader('Set-Cookie', cookies)
   }
-
   if (isBrowser()) {
     document.cookie = cookie.serialize(name, '', opts)
   }
-
   return {}
 }
-
-export default {
+exports.destroyCookie = destroyCookie
+exports.default = {
   set: setCookie,
   get: parseCookies,
   destroy: destroyCookie,
 }
+//# sourceMappingURL=index.js.map
